f2d8b7f5e926216e116898327c264492184f4f8e
Biandi Erdiputra <biandierdiputra@Biandis-MacBook-Pro-2.local>
2021-12-15 01:29:24 +0700

    edited css for auth, header, footer

--- components/auth/ForgotPassword.js
+++ components/auth/ForgotPassword.js
@@ -19,34 +19,36 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="container container-fluid">
-      <div className="row wrapper">
-        <div className="col-10 col-lg-5">
-          <form className="shadow-lg forgot" onSubmit={sendEmail}>
-            <h1 className="mb-3" style={{ textAlign: 'center' }}>
-              Forgot Password
-            </h1>
-            <div className="form-group">
-              <label htmlFor="email_field">Email</label>
-              <input
-                type="text"
-                id="email_field"
-                className="form-control"
-                value={email}
-                placeholder="Enter an email..."
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
+    <div className="auth-background">
+      <div className="container container-fluid">
+        <div className="row wrapper">
+          <div className="col-10 col-lg-5">
+            <form className="shadow-lg forgot" onSubmit={sendEmail}>
+              <h1 className="mb-3" style={{ textAlign: 'center' }}>
+                FORGOT PASSWORD
+              </h1>
+              <div className="form-group">
+                <label htmlFor="email_field">Email</label>
+                <input
+                  type="text"
+                  id="email_field"
+                  className="form-control"
+                  value={email}
+                  placeholder="Enter an email..."
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
 
-            <button
-              id="login_button"
-              type="submit"
-              className="btn btn-block py-3"
-              style={{ marginBottom: '10px' }}
-            >
-              Submit
-            </button>
-          </form>
+              <button
+                id="login_button"
+                type="submit"
+                className="btn btn-block py-3"
+                style={{ marginBottom: '10px' }}
+              >
+                SUBMIT
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
